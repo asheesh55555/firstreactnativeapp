f246bde7ee953ef1e24c8607213e148244b6244a
rakesh maurya <rakeshmaurya262@gmail.com>
2018-12-31 17:18:04 +0530

    first commit

---
+++ screens/AsheeshBananaScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class AsheeshBananaScreen extends React.Component {
+
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+  }
